d0fc6b641b0cf772c53510f658ac2be1af1cef36
dadanabdilah <abdilahdadan5@gmailcom>
2023-03-05 17:48:30 +0700

    delete login

--- sistem/app/Views/template.php
+++ sistem/app/Views/template.php
@@ -499,12 +499,7 @@
                                     <line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
                                 Cek Pesanan
                                 </a>
-                                <?php if ($users == false): ?>
-                                <a class="nav-item nav-link <?= $menu_active == 'Login' ? 'active' : ''; ?>" href="<?= base_url(); ?>/login">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path><polyline points="10 17 15 12 10 7"></polyline><line x1="15" y1="12" x2="3" y2="12"></line></svg>
-                                Login Member
-                                </a>
-                                <?php endif ?>
+    
                                 <?php if ($admin !== false): ?>
                                 <a class="nav-item nav-link" href="<?= base_url(); ?>/admin">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
